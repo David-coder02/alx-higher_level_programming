bd3090c9c3ced9e7ede5b85067deda21d0205944
David-coder02 <davidmushi88@gmail.com>
2021-09-30 04:00:14 -0700

    made change to task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""Square class definition"""
+""" Square class definition """
 
 
 class Square:
-    """Represents square
+    """ Represents square
 
     Attributes:
         __size (int): size of a side of the square
     """
     def __init__(self, size):
-        """Initializes a square
-
-        Args:
-            size (int): size of a side of the square
-
-        Returns: None
-        """            
+        """Initializes a square."""
         self.__size = size
