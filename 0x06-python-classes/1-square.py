c832fee8920edafb7dba3f4f44225e57d06079cf
David-coder02 <davidmushi88@gmail.com>
2021-09-30 03:18:24 -0700

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,5 +9,11 @@ class Square:
         __size (int): size of a side of the square
     """
     def __init__(self, size):
-        """Initializes the data."""            
+        """Initializes a square
+
+        Args:
+            size (int): size of a side of the square
+
+        Returns: None
+        """            
         self.__size = size
